8b21a9752e5fb03398068d53b3f1b69c3cc72122
heathersmiles <115672622+heathersmiles@users.noreply.github.com>
2023-01-18 15:32:59 -0800

    Update mit.js

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    colors: ['Silver Gray', 'Cardinal Red'],
+    mascot: "Tim the Beaver",
+    founded: 1861,
+    motto: 'Mens et Manus',
 };
